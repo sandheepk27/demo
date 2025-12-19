aeabe55f07d961b687bc7e84f52b7dd70a360905
sandheepk27 <sandheepk27@gmail.com>
2025-12-19 15:16:03 +0530

    added main1

---
+++ main1.py
@@ -0,0 +1,11 @@
+from typing import Any, List
+
+def isList(nums: List[Any]) -> bool:
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
+    return [nums]
+
+print(isList([1,1,2,3,3,4,5,6]))
